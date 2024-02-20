9de297ef7f839086d7db298ac997cbd71924f0dc
Oluwatobi Alani <adebayodeboraholuwatobi@gmail.com>
2024-02-20 13:09:45 +0100

    more files

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""program will assign a random signed number to the variable number each time it is executed."""
+
+import random
+number = random.randint(-10, 10)
+if number > 0:
+    print(f"{number} is positive")
+elif number == 0:
+    print(f"{number} is zero")
+else:
+    print(f"{number} is negative")
+
