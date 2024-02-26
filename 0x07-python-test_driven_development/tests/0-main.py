01bbd1c1f2f285db6f6f06b6159089ee6a037529
Oluwatobi Alani <adebayodeboraholuwatobi@gmail.com>
2024-02-26 17:26:03 +0100

    files on tests

---
+++ 0x07-python-test_driven_development/tests/0-main.py
@@ -0,0 +1,16 @@
+guillaume@ubuntu:~/0x07$ cat 0-main.py
+#!/usr/bin/python3
+add_integer = __import__('0-add_integer').add_integer
+
+print(add_integer(1, 2))
+print(add_integer(100, -2))
+print(add_integer(2))
+print(add_integer(100.3, -2))
+try:
+    print(add_integer(4, "School"))
+except Exception as e:
+    print(e)
+try:
+    print(add_integer(None))
+except Exception as e:
+    print(e)
